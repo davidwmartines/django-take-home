b853a1aabceb59bd65149631098dcf90fedd821e
davidwmartines <d5172@yahoo.com>
2020-12-22 10:57:02 -0600

    Add str methods to models.

--- transactions/models.py
+++ transactions/models.py
@@ -4,19 +4,35 @@ from django.db import models
 class Account(models.Model):
     name = models.TextField()
 
+    def __str__(self):
+        return self.name
+
 
 class Transaction(models.Model):
     amount = models.DecimalField(decimal_places=2, max_digits=9)
     trans_date = models.DateTimeField("transaction date")
-    from_account = models.ForeignKey(Account, related_name="from_account", on_delete=models.RESTRICT)
-    to_account = models.ForeignKey(Account, related_name="to_account", on_delete=models.RESTRICT)
+    from_account = models.ForeignKey(
+        Account, related_name="from_account", on_delete=models.RESTRICT
+    )
+    to_account = models.ForeignKey(
+        Account, related_name="to_account", on_delete=models.RESTRICT
+    )
+
+    def __str__(self):
+        return f"{self.trans_date} {self.amount} {self.to_account.name}  ({self.from_account.name})"
 
 
 class ExpenseCategory(models.Model):
     name = models.TextField()
+
+    def __str__(self):
+        return self.name
 
 
 class Expense(models.Model):
     transaction = models.ForeignKey(Transaction, on_delete=models.RESTRICT)
     category = models.ForeignKey(ExpenseCategory, on_delete=models.RESTRICT)
     amount = models.DecimalField(decimal_places=2, max_digits=9)
+
+    def __str__(self):
+        return f"{self.category} {self.amount} {self.transaction.trans_date}"
